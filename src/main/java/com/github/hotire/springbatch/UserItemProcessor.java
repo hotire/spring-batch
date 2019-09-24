6fa6c72efbcd159c54b6073aebfaafca8324c123
hotire <gngh0101@gmail.com>
2019-09-25 00:52:10 +0900

    Add UserItemProcessor for Implementation of ItemProcessor

--- src/main/java/com/github/hotire/springbatch/UserItemProcessor.java
+++ src/main/java/com/github/hotire/springbatch/UserItemProcessor.java
@@ -1,11 +1,20 @@
 package com.github.hotire.springbatch;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.ItemProcessor;
 
+@Slf4j
 public class UserItemProcessor implements ItemProcessor<User, User> {
 
   @Override
   public User process(User user) throws Exception {
-    return null;
+    final String firstName = user.getFirstName().toUpperCase();
+    final String lastName = user.getLastName().toUpperCase();
+
+    final User transformedUser = new User(firstName, lastName);
+
+    log.info("Converting ({}) into ({})", user, transformedUser);
+
+    return transformedUser;
   }
 }
