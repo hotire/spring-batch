bf5a43e1fdd4fb783f50fa90e80821fdf2c3b32c
hotire <gngh0101@gmail.com>
2019-09-25 00:50:38 +0900

    Add constructor of firstName, lastName

--- src/main/java/com/github/hotire/springbatch/User.java
+++ src/main/java/com/github/hotire/springbatch/User.java
@@ -4,14 +4,21 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PACKAGE)
 @Entity
+@Getter
 public class User {
   @Id
   @GeneratedValue
   private Long id;
   private String firstName;
   private String lastName;
+
+  public User(String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 }
