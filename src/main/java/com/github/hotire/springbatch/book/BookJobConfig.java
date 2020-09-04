b3a534e823f4236fcda4f52a8722c29e4dab091c
hotire <gngh0101@gmail.com>
2020-09-05 00:19:47 +0900

    Fix BookJobConfig

---
+++ src/main/java/com/github/hotire/springbatch/book/BookJobConfig.java
@@ -0,0 +1,9 @@
+package com.github.hotire.springbatch.book;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BookJobConfig {
+}
