2e0da92423bd4b3337ea4cd3529a55129bfd13d5
hotire <gngh0101@gmail.com>
2020-09-06 01:05:00 +0900

    Add BookReader Bean

--- src/main/java/com/github/hotire/springbatch/book/BookJobConfig.java
+++ src/main/java/com/github/hotire/springbatch/book/BookJobConfig.java
@@ -2,11 +2,20 @@ package com.github.hotire.springbatch.book;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManagerFactory;
 
 @Configuration
 @EnableBatchProcessing
 @RequiredArgsConstructor
 public class BookJobConfig {
 
+    private final EntityManagerFactory entityManagerFactory;
+
+    @Bean
+    public BookReader bookReader() {
+        return new BookReader(entityManagerFactory, 100, "SELECT b FROM Book b");
+    }
 }
