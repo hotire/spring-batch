07088cd8cda74df511cae0dfd00e86ee80e1a144
hotire <gngh0101@gmail.com>
2020-09-06 01:03:29 +0900

    Add entityManagerFactory to BookReader Constructor

--- src/main/java/com/github/hotire/springbatch/book/BookJobConfig.java
+++ src/main/java/com/github/hotire/springbatch/book/BookJobConfig.java
@@ -1,9 +1,12 @@
 package com.github.hotire.springbatch.book;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableBatchProcessing
+@RequiredArgsConstructor
 public class BookJobConfig {
+
 }
--- src/main/java/com/github/hotire/springbatch/book/BookReader.java
+++ src/main/java/com/github/hotire/springbatch/book/BookReader.java
@@ -2,8 +2,13 @@ package com.github.hotire.springbatch.book;
 
 import org.springframework.batch.item.database.JpaPagingItemReader;
 
+import javax.persistence.EntityManagerFactory;
+
 public class BookReader extends JpaPagingItemReader<Book> {
-    public BookReader(int pageSize) {
+
+    public BookReader(final EntityManagerFactory entityManagerFactory, final int pageSize, final String query) {
+        setEntityManagerFactory(entityManagerFactory);
         setPageSize(pageSize);
+        setQueryString(query);
     }
 }
