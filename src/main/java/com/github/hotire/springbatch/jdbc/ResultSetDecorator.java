070382a8abd8449fdacbbd1746716c4e2293350e
hotire <gngh0101@gmail.com>
2021-08-10 00:49:15 +0900

    Add ResultSet mode

--- src/main/java/com/github/hotire/springbatch/jdbc/ResultSetDecorator.java
+++ src/main/java/com/github/hotire/springbatch/jdbc/ResultSetDecorator.java
@@ -8,6 +8,10 @@ import lombok.experimental.Delegate;
 @RequiredArgsConstructor
 public class ResultSetDecorator implements ResultSet {
 
+    public static final int FETCH_FORWARD = ResultSet.FETCH_FORWARD;
+    public static final int FETCH_REVERSE = ResultSet.FETCH_REVERSE;
+    public static final int FETCH_UNKNOWN = ResultSet.FETCH_UNKNOWN;
+
     @Delegate
     private final ResultSet delegate;
 }
