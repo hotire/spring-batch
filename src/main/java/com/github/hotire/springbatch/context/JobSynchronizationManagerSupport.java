e1028e99f821e0eb447e577e8770f9dbd3a0ca9c
hotire <gngh0101@gmail.com>
2021-08-15 12:09:48 +0900

    Add JobSynchronizationManagerSupport

---
+++ src/main/java/com/github/hotire/springbatch/context/JobSynchronizationManagerSupport.java
@@ -0,0 +1,22 @@
+package com.github.hotire.springbatch.context;
+
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.jsr.configuration.support.BatchPropertyContext;
+import org.springframework.batch.core.scope.context.JobContext;
+import org.springframework.batch.core.scope.context.SynchronizationManagerSupport;
+
+/**
+ * @see org.springframework.batch.core.scope.context.JobSynchronizationManager
+ */
+public class JobSynchronizationManagerSupport extends SynchronizationManagerSupport<JobExecution, JobContext> {
+
+    @Override
+    protected void close(JobContext context) {
+        context.close();
+    }
+
+    @Override
+    protected JobContext createNewContext(JobExecution execution, BatchPropertyContext propertyContext) {
+        return new JobContext(execution);
+    }
+}
