306ccf43de02650e1bab5f8a8d5feecc39fade91
hotire <gngh0101@gmail.com>
2021-09-26 20:05:00 +0900

    Add partitionStep

--- src/main/java/com/github/hotire/springbatch/partitioner/PartitionerConfig.java
+++ src/main/java/com/github/hotire/springbatch/partitioner/PartitionerConfig.java
@@ -6,10 +6,14 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.partition.support.Partitioner;
+import org.springframework.batch.core.partition.support.SimplePartitioner;
 import org.springframework.batch.item.ParseException;
 import org.springframework.batch.item.UnexpectedInputException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import lombok.RequiredArgsConstructor;
 
@@ -33,11 +37,31 @@ public class PartitionerConfig {
 
     @Bean
     public Step partitionStep() throws UnexpectedInputException, MalformedURLException, ParseException {
-//        return steps.get("partitionStep")
-//                    .partitioner("slaveStep", partitioner())
-//                    .step(slaveStep())
-//                    .taskExecutor(taskExecutor())
-//                    .build();
+        return steps.get("partitionStep")
+                    .partitioner("slaveStep", partitioner())
+                    .step(slaveStep())
+                    .taskExecutor(taskExecutor())
+                    .build();
+    }
+
+    @Bean
+    public Step slaveStep() throws UnexpectedInputException, MalformedURLException, ParseException {
         return null;
     }
+
+    @Bean
+    public Partitioner partitioner() {
+        return new SimplePartitioner();
+    }
+
+    @Bean
+    public TaskExecutor taskExecutor() {
+        final ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setMaxPoolSize(5);
+        taskExecutor.setCorePoolSize(5);
+        taskExecutor.setQueueCapacity(5);
+        taskExecutor.afterPropertiesSet();
+        return taskExecutor;
+    }
+
 }
