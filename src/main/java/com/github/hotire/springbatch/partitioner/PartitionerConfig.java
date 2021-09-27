e1d042a60531218d4fe57454c2058a3625d538b8
hotire <gngh0101@gmail.com>
2021-09-27 09:52:01 +0900

    Simplify

--- src/main/java/com/github/hotire/springbatch/partitioner/PartitionerConfig.java
+++ src/main/java/com/github/hotire/springbatch/partitioner/PartitionerConfig.java
@@ -60,7 +60,6 @@ public class PartitionerConfig {
         taskExecutor.setMaxPoolSize(5);
         taskExecutor.setCorePoolSize(5);
         taskExecutor.setQueueCapacity(5);
-        taskExecutor.afterPropertiesSet();
         return taskExecutor;
     }
 
