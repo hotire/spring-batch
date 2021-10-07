b56bcd732cec2afa99584b7bf6cb825b1e0dd175
hotire <gngh0101@gmail.com>
2021-10-08 00:51:54 +0900

    Add CustomJdbcBatchItemWriter

---
+++ src/main/java/com/github/hotire/springbatch/writer/CustomJdbcBatchItemWriter.java
@@ -0,0 +1,6 @@
+package com.github.hotire.springbatch.writer;
+
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+
+public class CustomJdbcBatchItemWriter extends JdbcBatchItemWriter {
+}
---
+++ src/main/java/com/github/hotire/springbatch/writer/README.md
@@ -0,0 +1,6 @@
+# Writer
+
+## JdbcBatchItemWriter
+
+- https://jojoldu.tistory.com/339
+- https://wave1994.tistory.com/160
