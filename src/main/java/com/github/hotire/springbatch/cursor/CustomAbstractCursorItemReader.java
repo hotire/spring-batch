c2389fc914ec0db30a3e1092ba89216b31026a51
hotire <gngh0101@gmail.com>
2022-01-01 14:26:48 +0900

    Add BinaryResultsetReader

--- src/main/java/com/github/hotire/springbatch/cursor/CustomAbstractCursorItemReader.java
+++ src/main/java/com/github/hotire/springbatch/cursor/CustomAbstractCursorItemReader.java
@@ -1,6 +1,10 @@
 package com.github.hotire.springbatch.cursor;
 
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.jdbc.SQLWarningException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,5 +15,12 @@ import lombok.RequiredArgsConstructor;
 public class CustomAbstractCursorItemReader<T> extends JdbcCursorItemReader<T> {
 
     private final JdbcCursorItemReader<T> reader;
+
+    @Override
+    protected void handleWarnings(Statement statement) throws SQLWarningException,
+                                                              SQLException {
+        // error statement.getWarnings();
+        // ignoring warnings
+    }
 
 }
