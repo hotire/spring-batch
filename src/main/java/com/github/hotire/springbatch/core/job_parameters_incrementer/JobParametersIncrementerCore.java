81485e5a129ab3e9b5e6edecc8dcf19db52defcf
hotire <gngh0101@gmail.com>
2023-09-05 03:23:09 +0900

    Add JobParametersIncrementerCore

---
+++ src/main/java/com/github/hotire/springbatch/core/job_parameters_incrementer/JobParametersIncrementerCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springbatch.core.job_parameters_incrementer;
+
+import org.springframework.batch.core.JobParametersIncrementer;
+
+/**
+ * @see JobParametersIncrementer
+ */
+public class JobParametersIncrementerCore {
+
+}
