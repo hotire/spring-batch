e3cc68e64e19b9d99d984d2fbae86f78c727e512
hotire <gngh0101@gmail.com>
2023-09-06 19:07:18 +0900

    Add RunIdIncrementerCore

---
+++ src/main/java/com/github/hotire/springbatch/core/job_parameters_incrementer/RunIdIncrementerCore.java
@@ -0,0 +1,11 @@
+package com.github.hotire.springbatch.core.job_parameters_incrementer;
+
+
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+
+/**
+ * @see RunIdIncrementer
+ */
+public class RunIdIncrementerCore {
+
+}
