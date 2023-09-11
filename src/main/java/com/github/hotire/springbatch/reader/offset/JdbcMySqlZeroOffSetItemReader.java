dac8bb2e47a0be5096f235b46122c4817d0e7a9c
hotire <gngh0101@gmail.com>
2023-09-12 02:59:26 +0900

    Update JdbcMySqlZeroOffSetItemReader

--- src/main/java/com/github/hotire/springbatch/reader/offset/JdbcMySqlZeroOffSetItemReader.java
+++ src/main/java/com/github/hotire/springbatch/reader/offset/JdbcMySqlZeroOffSetItemReader.java
@@ -1,5 +1,6 @@
 package com.github.hotire.springbatch.reader.offset;
 
+import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.item.NonTransientResourceException;
 import org.springframework.batch.item.ParseException;
@@ -13,6 +14,8 @@ import org.springframework.batch.item.support.AbstractItemStreamItemReader;
 @RequiredArgsConstructor
 public class JdbcMySqlZeroOffSetItemReader<T, ID extends Comparable<ID>> extends AbstractItemStreamItemReader<T> {
 
+    private final DataSource dataSource;
+    private final String sql;
     private final int pageSize;
     private ID greaterThanId;
     private final IdMapper<T, ID> idMapper;
