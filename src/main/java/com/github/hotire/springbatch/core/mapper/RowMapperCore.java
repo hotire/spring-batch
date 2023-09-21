f8f19df16b23250c6799d6c1027e9aff26c620d2
hotire <gngh0101@gmail.com>
2023-09-22 01:51:06 +0900

    Add kotlin Data Class mapper

--- src/main/java/com/github/hotire/springbatch/core/mapper/RowMapperCore.java
+++ src/main/java/com/github/hotire/springbatch/core/mapper/RowMapperCore.java
@@ -2,9 +2,10 @@ package com.github.hotire.springbatch.core.mapper;
 
 import org.springframework.jdbc.core.RowMapper;
 
-
 /**
  * @see RowMapper
+ * kotlin Data Class
+ * https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/jdbc/core/DataClassRowMapper.html
  */
 public class RowMapperCore {
 
