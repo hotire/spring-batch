b819d1bb8dbb8a3a41040dd2f06fd50b7a22943d
hotire <gngh0101@gmail.com>
2023-11-17 03:13:17 +0900

    Add JdbcPagingItemReaderCore

---
+++ src/main/java/com/github/hotire/springbatch/core/reader/JdbcPagingItemReaderCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springbatch.core.reader;
+
+import import org.springframework.batch.item.database.JdbcPagingItemReader;
+
+/**
+ * @see JdbcPagingItemReader
+ */
+public class JdbcPagingItemReaderCore {
+
+}
