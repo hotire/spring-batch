9cba0a157a485aa5df3b4684beee7f766db2a6dd
hotire <gngh0101@gmail.com>
2023-11-18 22:48:40 +0900

    Add PagingQueryProviderCore

---
+++ src/main/java/com/github/hotire/springbatch/core/reader/query/PagingQueryProviderCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springbatch.core.reader.query;
+
+import org.springframework.batch.item.database.PagingQueryProvider;
+
+/**
+ * @see PagingQueryProvider
+ */
+public class PagingQueryProviderCore {
+
+}
