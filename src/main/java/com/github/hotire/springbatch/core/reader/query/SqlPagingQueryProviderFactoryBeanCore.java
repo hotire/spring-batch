7f2d8d5542e417993da50f7452222dcd12ee6c17
hotire <gngh0101@gmail.com>
2023-11-18 22:52:21 +0900

    Add SqlPagingQueryProviderFactoryBeanCore

---
+++ src/main/java/com/github/hotire/springbatch/core/reader/query/SqlPagingQueryProviderFactoryBeanCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springbatch.core.reader.query;
+
+import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean
+
+/**
+ * @see SqlPagingQueryProviderFactoryBean
+ */
+public class SqlPagingQueryProviderFactoryBeanCore {
+
+}
