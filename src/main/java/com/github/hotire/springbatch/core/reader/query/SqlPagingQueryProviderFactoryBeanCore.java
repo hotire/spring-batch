2ab5f30dbf3ee6d8cc9008b1222a5b6f0f7ad1c3
hotire <gngh0101@gmail.com>
2023-11-19 23:37:51 +0900

    SqlPagingQueryProviderFactoryBean#providers

--- src/main/java/com/github/hotire/springbatch/core/reader/query/SqlPagingQueryProviderFactoryBeanCore.java
+++ src/main/java/com/github/hotire/springbatch/core/reader/query/SqlPagingQueryProviderFactoryBeanCore.java
@@ -1,10 +1,19 @@
 package com.github.hotire.springbatch.core.reader.query;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.batch.item.database.support.AbstractSqlPagingQueryProvider;
 import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
+import org.springframework.batch.support.DatabaseType;
 
 /**
  * @see SqlPagingQueryProviderFactoryBean
  */
 public class SqlPagingQueryProviderFactoryBeanCore {
+
+    /**
+     * @see SqlPagingQueryProviderFactoryBean#providers
+     */
+    private Map<DatabaseType, AbstractSqlPagingQueryProvider> providers = new HashMap<>();
 
 }
