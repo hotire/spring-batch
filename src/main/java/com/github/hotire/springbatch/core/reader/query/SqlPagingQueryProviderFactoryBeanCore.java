2e57804dfedc3935d927efa288e33786173478af
hotire <gngh0101@gmail.com>
2023-11-19 23:38:47 +0900

    SqlPagingQueryProviderFactoryBean#getObject()

--- src/main/java/com/github/hotire/springbatch/core/reader/query/SqlPagingQueryProviderFactoryBeanCore.java
+++ src/main/java/com/github/hotire/springbatch/core/reader/query/SqlPagingQueryProviderFactoryBeanCore.java
@@ -2,6 +2,7 @@ package com.github.hotire.springbatch.core.reader.query;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.batch.item.database.PagingQueryProvider;
 import org.springframework.batch.item.database.support.AbstractSqlPagingQueryProvider;
 import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
 import org.springframework.batch.support.DatabaseType;
@@ -15,5 +16,13 @@ public class SqlPagingQueryProviderFactoryBeanCore {
      * @see SqlPagingQueryProviderFactoryBean#providers
      */
     private Map<DatabaseType, AbstractSqlPagingQueryProvider> providers = new HashMap<>();
+
+
+    /**
+     * @see SqlPagingQueryProviderFactoryBean#getObject()
+     */
+    public PagingQueryProvider getObject() throws Exception {
+        return null;
+    }
 
 }
