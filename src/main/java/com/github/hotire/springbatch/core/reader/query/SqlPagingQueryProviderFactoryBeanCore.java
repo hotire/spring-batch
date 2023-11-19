0c5f8efc41569d2fa1070496b058db87b75cc2c4
hotire <gngh0101@gmail.com>
2023-11-19 23:37:35 +0900

    Fix typo

--- src/main/java/com/github/hotire/springbatch/core/reader/query/SqlPagingQueryProviderFactoryBeanCore.java
+++ src/main/java/com/github/hotire/springbatch/core/reader/query/SqlPagingQueryProviderFactoryBeanCore.java
@@ -1,6 +1,6 @@
 package com.github.hotire.springbatch.core.reader.query;
 
-import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean
+import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
 
 /**
  * @see SqlPagingQueryProviderFactoryBean
