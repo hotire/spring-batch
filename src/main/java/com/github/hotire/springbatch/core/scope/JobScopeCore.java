f0dd6a1a429306e00e554c4015daff2d3760f707
hotire <gngh0101@gmail.com>
2024-02-07 21:11:15 +0900

    Add JobScopeCore

---
+++ src/main/java/com/github/hotire/springbatch/core/scope/JobScopeCore.java
@@ -0,0 +1,12 @@
+package com.github.hotire.springbatch.core.scope;
+
+import org.springframework.batch.core.configuration.annotation.JobScope;
+
+/**
+ * @see JobScope
+ * @see org.springframework.batch.core.scope.JobScope
+ */
+@JobScope
+public class JobScopeCore {
+
+}
