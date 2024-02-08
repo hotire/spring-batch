edf8cc21625783cf1fc5b3ba3946bb3e42ecf7c4
hotire <gngh0101@gmail.com>
2024-02-08 20:27:05 +0900

    Add ScopeCore

--- src/main/java/com/github/hotire/springbatch/core/scope/JobScopeCore.java
+++ src/main/java/com/github/hotire/springbatch/core/scope/JobScopeCore.java
@@ -5,6 +5,7 @@ import org.springframework.batch.core.configuration.annotation.JobScope;
 /**
  * @see JobScope
  * @see org.springframework.batch.core.scope.JobScope
+ * @see org.springframework.beans.factory.config.Scope
  */
 @JobScope
 public class JobScopeCore {
---
+++ src/main/java/com/github/hotire/springbatch/core/scope/ScopeCore.java
@@ -0,0 +1,11 @@
+package com.github.hotire.springbatch.core.scope;
+
+
+import  org.springframework.beans.factory.config.Scope;
+
+/**
+ * @see Scope
+ */
+public class ScopeCore {
+
+}
