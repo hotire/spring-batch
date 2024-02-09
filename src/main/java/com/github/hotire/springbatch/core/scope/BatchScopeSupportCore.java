fa5dec579238581228bcf2c8954b0f0d995a2733
hotire <gngh0101@gmail.com>
2024-02-09 23:08:16 +0900

    Add BatchScopeSupportCore

--- src/main/java/com/github/hotire/springbatch/core/scope/BatchScopeSupportCore.java
+++ src/main/java/com/github/hotire/springbatch/core/scope/BatchScopeSupportCore.java
@@ -1,10 +1,24 @@
 package com.github.hotire.springbatch.core.scope;
 
 import org.springframework.batch.core.scope.BatchScopeSupport;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 
 /**
  * @see BatchScopeSupport
  */
-public class BatchScopeSupportCore {
+public abstract class BatchScopeSupportCore {
 
+    /**
+     * @see BatchScopeSupport#postProcessBeanFactory(ConfigurableListableBeanFactory) 
+     */
+    public void postProcessBeanFactory(
+        ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        
+    }
+
+    /**
+     * @see BatchScopeSupport#getTargetNamePrefix()
+     */
+    public abstract String getTargetNamePrefix();
 }
