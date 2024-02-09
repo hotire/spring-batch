dab3f7e9d0c45116ba6c1c3c5cf96145259cfbf4
hotire <gngh0101@gmail.com>
2024-02-09 23:03:56 +0900

    Add ScopeCore

--- src/main/java/com/github/hotire/springbatch/core/scope/ScopeCore.java
+++ src/main/java/com/github/hotire/springbatch/core/scope/ScopeCore.java
@@ -1,11 +1,16 @@
 package com.github.hotire.springbatch.core.scope;
 
 
+import org.springframework.beans.factory.ObjectFactory;
 import  org.springframework.beans.factory.config.Scope;
 
 /**
  * @see Scope
  */
-public class ScopeCore {
+public interface ScopeCore {
 
+    /**
+     * @see Scope#get(String, ObjectFactory) 
+     */
+    Object get(String name, ObjectFactory<?> objectFactory);
 }
