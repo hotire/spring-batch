f8d5fcef9af9d1aaa5736eaaacc2e95f331dbac4
hotire <gngh0101@gmail.com>
2024-03-05 01:40:09 +0900

    HelloJobConfig Parameter 구현

--- src/main/java/com/github/hotire/springbatch/rest/HelloJobConfig.java
+++ src/main/java/com/github/hotire/springbatch/rest/HelloJobConfig.java
@@ -1,24 +1,44 @@
 package com.github.hotire.springbatch.rest;
 
+import com.github.hotire.springbatch.AbstractJobConfig;
+import javax.annotation.PostConstruct;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.github.hotire.springbatch.AbstractJobConfig;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class HelloJobConfig extends AbstractJobConfig {
+
+    private final Parameter parameter;
+
+    @Getter
+    @Setter
+    public static class Parameter {
+        private String name;
+
+        @PostConstruct
+        public void init() {
+            log.info("[HelloJobConfig-Parameter] init");
+        }
+    }
+
+    @PostConstruct
+    public void init() {
+        log.info("[HelloJobConfig] init");
+    }
+
     @Bean
     public Job helloJob() {
         return registerJob(getJobBuilderFactory().get("helloJob")
@@ -31,11 +51,19 @@ public class HelloJobConfig extends AbstractJobConfig {
     @Bean
     @JobScope
     public Step helloStep(@Value("#{jobParameters['sampleProperty']}") String sampleProperty){
+        log.info("[HelloJobConfig] parameter name : {}", parameter.getName());
         return getStepBuilderFactory().get("helloStep")
                                       .tasklet((stepContribution, chunkContext) -> {
                                           log.info("hello");
                                           log.info("sampleProperty : {}", sampleProperty);
                                           return RepeatStatus.FINISHED;
                                       }).build();
+    }
+
+
+    @Bean
+    @JobScope
+    public Parameter parameter() {
+        return new Parameter();
     }
 }
