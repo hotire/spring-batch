4a729a11ad6c5ee4b09c001153b754fb19b6ccee
hotire <gngh0101@gmail.com>
2024-04-18 02:33:28 +0900

    Add ResourcelessTransactionManagerCore

---
+++ src/main/java/com/github/hotire/springbatch/core/transaction/ResourcelessTransactionManagerCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springbatch.core.transaction;
+
+import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
+
+/**
+ * @see ResourcelessTransactionManager
+ */
+public class ResourcelessTransactionManagerCore {
+
+}
