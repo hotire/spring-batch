b808f5c7f7e526da7d263df0f99479f983ffb02f
hotire <gngh0101@gmail.com>
2024-04-19 21:36:21 +0900

    Update ResourcelessTransactionManagerCore

--- src/main/java/com/github/hotire/springbatch/core/transaction/ResourcelessTransactionManagerCore.java
+++ src/main/java/com/github/hotire/springbatch/core/transaction/ResourcelessTransactionManagerCore.java
@@ -7,4 +7,22 @@ import org.springframework.batch.support.transaction.ResourcelessTransactionMana
  */
 public class ResourcelessTransactionManagerCore {
 
+    private static class ResourcelessTransaction {
+
+        private boolean active = false;
+
+        public boolean isActive() {
+            return active;
+        }
+
+        public void begin() {
+            active = true;
+        }
+
+        public void clear() {
+            active = false;
+        }
+
+    }
+
 }
