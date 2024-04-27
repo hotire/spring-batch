fda64afb51adaf12e607fe320ccce4ce999fb846
hotire <gngh0101@gmail.com>
2024-04-27 12:42:25 +0900

    Add  com.zaxxer.hikari.pool.HikariPool.HouseKeeper#run()

--- src/main/java/com/github/hotire/springbatch/core/hikari/HouseKeeperCore.java
+++ src/main/java/com/github/hotire/springbatch/core/hikari/HouseKeeperCore.java
@@ -5,4 +5,11 @@ package com.github.hotire.springbatch.core.hikari;
  */
 public class HouseKeeperCore {
 
+    /**
+     * @see com.zaxxer.hikari.pool.HikariPool.HouseKeeper#run()
+     */
+    public void run() {
+
+    }
+
 }
